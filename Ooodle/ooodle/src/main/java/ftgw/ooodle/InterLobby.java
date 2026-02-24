8904f147e9ca2b03e9912692b67f6918f5addc12
D0maru <20martin2025@gmail.com>
2026-02-24 09:55:28 -0500

    el .java que contiene todas las funciones de los botones que iran dentro de la interfaz del lobby de usuario

---
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/InterLobby.java
@@ -0,0 +1,15 @@
+package ftgw.ooodle;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.layout.AnchorPane;
+
+public class InterLobby {
+
+    @FXML
+    private BarChart<?, ?> HistoEstad;
+
+    @FXML
+    private AnchorPane PanelInterfaz;
+
+}
