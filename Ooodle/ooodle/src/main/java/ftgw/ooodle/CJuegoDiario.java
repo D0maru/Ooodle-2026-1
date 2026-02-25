5773e1b300e2b5be75c10791db24dce36fa542c1
D0maru <20martin2025@gmail.com>
2026-02-25 17:52:36 -0500

    Agregue una interfaz con controlador para donde estaria destinado el juego diario

---
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/CJuegoDiario.java
@@ -0,0 +1,15 @@
+package ftgw.ooodle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class CJuegoDiario {
+
+    @FXML
+    private AnchorPane PanelBase;
+
+    @FXML
+    private Label letrero;
+
+}
