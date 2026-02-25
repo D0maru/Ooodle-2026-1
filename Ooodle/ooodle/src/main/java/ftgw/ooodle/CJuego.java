fde326f5043ec2c340f8b403c459cf225fea2718
D0maru <20martin2025@gmail.com>
2026-02-25 17:51:54 -0500

    Simplemente puse a prueba nombres y ubicacion de carpeta de donde iria el controlador .java y una interfaz cualquiera tomando la interfaz dedicada al teclado como ejemplo

---
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/CJuego.java
@@ -0,0 +1,50 @@
+package ftgw.ooodle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CJuego {
+
+    @FXML
+    private Button BCheck;
+
+    @FXML
+    private Button BDel;
+
+    @FXML
+    private Button BT1;
+
+    @FXML
+    private Button BT2;
+
+    @FXML
+    private Button BT3;
+
+    @FXML
+    private Button BT4;
+
+    @FXML
+    private Button BT5;
+
+    @FXML
+    private Button BT6;
+
+    @FXML
+    private Button BT7;
+
+    @FXML
+    private Button BT8;
+
+    @FXML
+    private Button BT9;
+
+    @FXML
+    private Button BTRestart;
+
+    @FXML
+    void Click(ActionEvent event) {
+
+    }
+
+}
