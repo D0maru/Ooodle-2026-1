be94a402f3d02fc2702610ac19fedbe8ba4f9e9b
Aleja <maltrujillo@poligran.edu.co>
2026-02-26 10:49:36 -0500

    Aqui puse la interfaz de reglas, la idea es que desde la interfaz Lobby haya un boton se pueda acceder a las reglas y volver a la interfaz de inicio

---
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/CReglas.java
@@ -0,0 +1,15 @@
+package ftgw.ooodle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class CReglas {
+
+    @FXML
+    private Pane PanelBase;
+
+    @FXML
+    private Label labeltitulo;
+
+}
