d828c9f0436a0e16e6ae3705c04194e3bd393271
D0maru <20martin2025@gmail.com>
2026-02-26 11:28:10 -0500

    Agregue la funcion al boton volver en la interfaz de reglas para volver a la pantalla Lobby

--- Ooodle/ooodle/src/main/java/ftgw/ooodle/CReglas.java
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/CReglas.java
@@ -1,7 +1,14 @@
 package ftgw.ooodle;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class CReglas {
@@ -10,6 +17,29 @@ public class CReglas {
     private Pane PanelBase;
 
     @FXML
+    private Button botonVolver;
+
+    @FXML
     private Label labeltitulo;
+
+    @FXML
+    void volverLobby(ActionEvent event) {
+        try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Lobby.fxml"));
+        Parent root = loader.load();
+
+        PanelBase.getChildren().clear();
+        PanelBase.getChildren().add(root);
+
+        AnchorPane.setTopAnchor(root, 0.0);
+        AnchorPane.setBottomAnchor(root, 0.0);
+        AnchorPane.setLeftAnchor(root, 0.0);
+        AnchorPane.setRightAnchor(root, 0.0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
