097992fa0dc0503dc85de6c104da80243e7d13e4
Sergio_Acero <sergioacero205@gmail.com>
2026-03-05 21:12:34 -0500

    Agrege el boton cambiar la dificultad
    En este caso lo que hice fue agregar en la interfaz lobby.fxml el boton de cambiar dificultar,ademas de esto en el controlador de esta interfaz(interLobby.java) agrege el metodo para que darle click al circulo se mueva y se cambie de forma correcta el label

--- Ooodle/ooodle/src/main/java/ftgw/ooodle/InterLobby.java
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/InterLobby.java
@@ -2,13 +2,18 @@ package ftgw.ooodle;
 
 import java.io.IOException;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 public class InterLobby {
 
@@ -39,6 +44,32 @@ public class InterLobby {
             e.printStackTrace();
         }
 
+    }
+    @FXML
+    private Circle circuloDificultad;
+
+    @FXML
+    private Label lblRango;
+
+    private boolean modo12 = false;
+    @FXML
+    void cambiarDificultad(MouseEvent event) {
+        // Creamos la animación (200 milisegundos de duración)
+        TranslateTransition animation = new TranslateTransition(Duration.millis(200), circuloDificultad);
+        //if para mover el circulo a la derecha o a la izquierda dependiendo del modo que escoja el usuario
+        if (!modo12) {
+            // Mover a la derecha para modo 1-12
+            animation.setToX(22); 
+            lblRango.setText("Numbers 1 to 12");
+            modo12 = true;
+        } else {
+            // Volver a la izquierda para modo 1-9
+            animation.setToX(0);
+            lblRango.setText("Numbers 1 to 9");
+            modo12 = false;
+        }
+        
+        animation.play(); // Ejecuta el movimiento
     }
 
 }
