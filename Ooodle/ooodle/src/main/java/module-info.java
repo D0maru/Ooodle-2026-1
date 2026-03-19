cf4e04278bcb5cdccf4b650811187fdc667f779b
Aleja <maltrujillo@poligran.edu.co>
2026-03-19 11:43:34 -0500

    cambio de donde los fxml pueden obtener sus controladores

--- Ooodle/ooodle/src/main/java/module-info.java
+++ Ooodle/ooodle/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ftgw.ooodle {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens ftgw.ooodle to javafx.fxml;
     opens ftgw.ooodle.Controladores to javafx.fxml;
