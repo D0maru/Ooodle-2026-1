9020564cd422a09bffb1dc7ab779b88e0369f8e2
Aleja <maltrujillo@poligran.edu.co>
2026-03-19 11:16:29 -0500

    Cambios en las rutas para que se muestren bien despues de moficiaciones mal ubicadas

--- Ooodle/ooodle/src/main/java/ftgw/ooodle/Controladores/CJuegoDiarioDificil.java
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/Controladores/CJuegoDiarioDificil.java
@@ -1,6 +1,135 @@
 package ftgw.ooodle.Controladores;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CJuegoDiarioDificil {
+
+    @FXML
+    private Button BtnRPP;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Button btn10;
+
+    @FXML
+    private Button btn11;
+
+    @FXML
+    private Button btn12;
+
+    @FXML
+    private Button btn2;
+
+    @FXML
+    private Button btn3;
+
+    @FXML
+    private Button btn4;
+
+    @FXML
+    private Button btn5;
+
+    @FXML
+    private Button btn6;
+
+    @FXML
+    private Button btn7;
+
+    @FXML
+    private Button btn8;
+
+    @FXML
+    private Button btn9;
+
+    @FXML
+    private Button btnRestart;
+
+    @FXML
+    private Button btncheck;
+
+    @FXML
+    private Button btndel;
+
+    @FXML
+    private TextField num1;
+
+    @FXML
+    private TextField num1f2;
+
+    @FXML
+    private TextField num1f3;
+
+    @FXML
+    private TextField num1f4;
+
+    @FXML
+    private TextField num1f5;
+
+    @FXML
+    private TextField num1f6;
+
+    @FXML
+    private TextField num2;
+
+    @FXML
+    private TextField num2f2;
+
+    @FXML
+    private TextField num2f3;
+
+    @FXML
+    private TextField num2f4;
+
+    @FXML
+    private TextField num2f5;
+
+    @FXML
+    private TextField num2f6;
+
+    @FXML
+    private TextField num3;
+
+    @FXML
+    private TextField num3f2;
+
+    @FXML
+    private TextField num3f3;
+
+    @FXML
+    private TextField num3f4;
+
+    @FXML
+    private TextField num3f5;
+
+    @FXML
+    private TextField num3f6;
+
+    @FXML
+    private TextField num4;
+
+    @FXML
+    private TextField num4f2;
+
+    @FXML
+    private TextField num4f3;
+
+    @FXML
+    private TextField num4f4;
+
+    @FXML
+    private TextField num4f5;
+
+    @FXML
+    private TextField num4f6;
+
+    @FXML
+    void volverAlLobby(ActionEvent event) {
+
+    }
 
 }
--- Ooodle/ooodle/src/main/java/ftgw/ooodle/Controladores/CVictoriaDiario.java
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/Controladores/CVictoriaDiario.java
@@ -1,36 +1,17 @@
 package ftgw.ooodle.Controladores;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class CVictoriaDiario {
 
     @FXML
-    private Button botonJugardenuevo;
-  
+    private Button botonRegresoLobby;
+
     @FXML
-    void jugar(ActionEvent event) {
-        try {
-        
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("JuegoDiarioFacil.fxml"));
-            Parent root = loader.load();
-            
-            Stage stage = (Stage)botonJugardenuevo.getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    void Volver(ActionEvent event) {
 
     }
 
+}
