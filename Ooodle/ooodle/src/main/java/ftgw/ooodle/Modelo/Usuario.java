2d1c5c116ebe9956a7ce6fcc616b7984b7878a03
D0maru <20martin2025@gmail.com>
2026-04-29 16:59:25 -0500

    Aqui puse el constructor de la clase usuario

--- Ooodle/ooodle/src/main/java/ftgw/ooodle/Modelo/Usuario.java
+++ Ooodle/ooodle/src/main/java/ftgw/ooodle/Modelo/Usuario.java
@@ -4,4 +4,10 @@ public class Usuario {
     public int id;
     private String nickname;
     private boolean juegoDiario;
+    
+    public Usuario(int id, String nickname, boolean juegoDiario) {
+        this.id = id;
+        this.nickname = nickname;
+        this.juegoDiario = juegoDiario;
+    }
 }
